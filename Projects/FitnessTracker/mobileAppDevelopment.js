bba5a33579f565510cd44557d0c1cc4125303b33
janeschmidt <jane.schmidt123@gmail.com>
2023-01-14 21:49:37 -0500

    Adding project pages and updating app page

--- Projects/FitnessTracker/mobileAppDevelopment.js
+++ Projects/FitnessTracker/mobileAppDevelopment.js
@@ -1,28 +1,48 @@
-const img = document.querySelectorAll('img');
-
-img.forEach(image => {
-    image.addEventListener('mouseover', blurPhoto(image.id));
-});
-
-function blurPhoto(id) {
-  console.log(id);
-}
-
 function init(){
-  console.log("hi");
-  document.getElementById("addWorkout").mouseover = function(){
-    console.log("1");
-    //document.getElementById("addWorkout").src = "blurredWorkout.png";
-  }
-  document.getElementById("tracker").mouseover = function(){
-    console.log("2");
-  }
-  document.getElementById("stats").mouseover = function(){
-    console.log("3");
-  }
-  document.getElementById("table").mouseover = function(){
-    console.log("4");
-  }
+  const img = document.querySelectorAll('img');
 
+  img.forEach(image => {
+      image.addEventListener("mouseenter", ()=> {
+        switch(image.id){
+          case "addWorkout" :
+            image.src = "addWorkoutBlurred.jpg";
+            break;
+          case "stats" :
+            image.src = "statisticsBlurred.png";
+            console.log("1");
+            break;
+          case "table" :
+            image.src = "tableBlurred.png";
+            console.log("2");
+            break;
+          case "tracker" :
+            image.src = "trackerBlurred.png";
+            console.log("3");
+            break;
+        }
+
+      });
+  });
+
+  img.forEach(image => {
+    image.addEventListener("mouseleave", ()=>{
+      switch(image.id){
+        case "addWorkout" :
+          image.src = "addWorkout.png";
+          break;
+        case "stats" :
+          image.src = "statistics.png";
+          break;
+        case "table" :
+          image.src = "workoutsTable.png";
+          break;
+        case "tracker" :
+          image.src = "tracker.png";
+          break;
+      }
+    });
+});
 }
+
+
 init();
