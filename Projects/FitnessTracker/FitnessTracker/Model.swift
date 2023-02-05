33515592e6a1321c8ad492b244e5e9d1c815c50c
JaneSchmidt <78976686+JaneSchmidt@users.noreply.github.com>
2023-02-05 17:18:37 -0500

    Update Model.swift

--- Projects/FitnessTracker/FitnessTracker/Model.swift
+++ Projects/FitnessTracker/FitnessTracker/Model.swift
@@ -21,21 +21,27 @@ class Model: NSObject{
     var currentMiles: String=""
     var currentType: String=""
 
+    // adds each workout as an Item containing all necessary information to the myData array
     func addEvent(aType: String, aDate: String, aMile: String, aDuration: String, aNotes: String){
         self.myData.append(Item(someType: aType, someDate: aDate, someMile: aMile, someDuration: aDuration, someNotes: aNotes))
     }
     
+    // gets number of workout entries to determine number of cells in table
     func getDataCount() -> Int{
         return myData.count
     }
+ 
+    // returns a workout as an Item
     func returnEvent() -> Item {
         return myData[myData.count - 1]
     }
     
+    // returns entire array of saved workouts
     func getItemArr() -> [Item]{
         return myData
     }
     
+    // returns the distance traveled from the last workout of the current type
     func getLastMiles() -> String {
         var prev = "0"
         for i in stride(from: myData.count-2, through: 0, by: -1){
@@ -47,6 +53,7 @@ class Model: NSObject{
         return prev
     }
     
+    //returns the duration from the last wrokout of the current type
     func getLastDuration() -> String {
         var prev = "0"
         for i in stride(from: myData.count-2, to: -1, by: -1){
@@ -58,34 +65,41 @@ class Model: NSObject{
         return prev
     }
     
+    // initializes the myData array with the past saved workout entries
     func initializeArr(savedArr:[Item]){
         myData=savedArr
-        print(myData)
+        //print(myData)
     }
     
+    // stores the time and miles into the current variables
     func storeTimeAndMiles(cTime: String, cMiles: String){
         currentTime=cTime
         currentMiles=cMiles
-        print("store time \(currentTime) store miles \(currentMiles)\n")
     }
+ 
+    // sets the store type to String
     func storeType(cType: String){
         currentType=cType
     }
+ 
+    // returns String as the type
     func getCurrentType()->String{
         return currentType
     }
+ 
+    // gets the current time from a workout
     func getCurrentTime()->String{
-        print("get time \(currentTime)\n")
         return currentTime
     }
+ 
+    // gets the current distance traveled during the workout
     func getCurrentMiles()->String{
-        print("get time \(currentMiles)\n")
         return currentMiles
     }
   
 }
 
-
+// creating the Item class to easily store a workout with all necessary data added as components of the class
 class Item: NSObject, Codable{
     
     var type: String
@@ -102,6 +116,7 @@ class Item: NSObject, Codable{
         self.notes = someNotes
     }
     
+    // adding some get functions
     func getType() -> String {
         return self.type
     }
