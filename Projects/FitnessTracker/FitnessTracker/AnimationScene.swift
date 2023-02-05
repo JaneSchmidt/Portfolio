0133a2286ea315d1d60214e93c4d5d32b60224e0
JaneSchmidt <78976686+JaneSchmidt@users.noreply.github.com>
2023-02-05 16:44:44 -0500

    Update AnimationScene.swift

--- Projects/FitnessTracker/FitnessTracker/AnimationScene.swift
+++ Projects/FitnessTracker/FitnessTracker/AnimationScene.swift
@@ -16,21 +16,31 @@ import Foundation
 import SpriteKit
 
 
+// Declares the Animation class as a SKScene
 class AnimationScene: SKScene{
     
+    // initializing all the variables to a default value
     var type : String = ""
     var image = SKSpriteNode(imageNamed: "workingOutGif.gif")
     var gifTextures: [SKTexture] = [];
     let myAppDelegate = UIApplication.shared.delegate as! AppDelegate
+ 
+    // override the didMove function to add functionality when the scene is presented by a view
     override func didMove(to view: SKView) {
+     
+        // create an instance of the Model class to get the current type of workout
         let myModel = myAppDelegate.myModel
         self.type=myModel.getCurrentType()
         print("animation type: \(self.type)")
+     
+        // case statement to display the correct Gif using the current type of workout
         switch self.type {
         case "biking":
+            // converts each image to an SKTexture and adds it to the gifTextures array
             for i in 1...12 {
                 gifTextures.append(SKTexture(imageNamed: "bikingGif\(i)"));
             }
+            // loops through and displays the SKTextures until the scene is no longer being presented
             image.run(SKAction.repeatForever(SKAction.animate(with: gifTextures, timePerFrame: 0.125)));
             
         case "walking" :
@@ -52,6 +62,7 @@ class AnimationScene: SKScene{
             image.run(SKAction.repeatForever(SKAction.animate(with: gifTextures, timePerFrame: 0.125)));
         }
         
+        // sets the image parameters and adds scene to the SKSpriteNode
         image.size = frame.size
         image.position=CGPoint(x: self.frame.midX,y: self.frame.midY)
         self.addChild(image)
