fb79c8a5a00983ad5a845d4c7e035a9874142eaf
JaneSchmidt <78976686+JaneSchmidt@users.noreply.github.com>
2023-02-05 16:18:44 -0500

    Update SevSegDisplay.sv

--- Projects/ElevatorCtrl/SevSegDisplay.sv
+++ Projects/ElevatorCtrl/SevSegDisplay.sv
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 
+// module used to declare the inputs and outputs the seven segment display will use
 module SevSegDisplay(
     input   [1:0] floorSel,
     input         door, 
@@ -8,15 +9,19 @@ module SevSegDisplay(
     output logic [3:0] select  
 );
 
+// process automatically executed at time zero 
 always_comb begin
 select = 4'b1110;
 segments = 7'b1000011;
 
+// tests if the door is opened or closed and lights up corresponding segments
 if(door == 1'h0)
     segments = 7'b0100011;
 else
     segments = 7'b1000011;
 
+// tests which floor the elevator is on to determine which of the four digits 
+// will display the elevator
 if(floorSel == 2'h0) 
     select = 4'b1110;
 if(floorSel == 2'h1) 
