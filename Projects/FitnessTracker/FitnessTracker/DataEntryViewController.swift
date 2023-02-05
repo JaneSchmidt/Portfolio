da07b44d761c193f111adf03a62db303251bdb33
JaneSchmidt <78976686+JaneSchmidt@users.noreply.github.com>
2023-02-05 17:02:13 -0500

    Update DataEntryViewController.swift

--- Projects/FitnessTracker/FitnessTracker/DataEntryViewController.swift
+++ Projects/FitnessTracker/FitnessTracker/DataEntryViewController.swift
@@ -28,45 +28,48 @@ class DataEntryViewController: UIViewController {
     @IBOutlet weak var myNotes: UITextView!
     @IBOutlet weak var myMiles: UITextField!
 
-    
     @IBAction func addNewReminder(_ sender: Any){
         
+        // creates an instance of the model class using the app delegate 
         let myAppDelegate = UIApplication.shared.delegate as! AppDelegate
         let myModel = myAppDelegate.myModel
+        
+        // disables keyboard for the user
         self.myType.resignFirstResponder()
         self.myMinutes.resignFirstResponder()
         self.myNotes.resignFirstResponder()
         self.myMiles.resignFirstResponder()
         
+        // initializes each variable to the text entered by user, default is an empty string
         let type: String = self.myType.text ?? ""
         let minutes: String = self.myMinutes.text ?? ""
         let miles: String = self.myMiles.text ?? ""
         let notes: String = self.myNotes.text ?? ""
         let date = NSDate().description
-            //.description
         
+        // resetting each text field and view back to empty strings
         self.myType.text = ""
         self.myMinutes.text = ""
         self.myMiles.text = ""
         self.myNotes.text = ""
         
+        // add user entered workout to the workout table using the addEvent function in the Model class
         myModel.addEvent(aType: type, aDate: date, aMile: miles, aDuration: minutes, aNotes: notes)
         print(myModel.returnEvent())
         
+        //persistent data retrieval
         do {
             let fm = FileManager.default
             let docsurl = try fm.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
                     
-            //print("retrieving saved plist array of Items")
+            // retrieving saved plist array of Items
             let itemArr = myModel.getItemArr()
             let arrFile = docsurl.appendingPathComponent("itemArr.plist")
             let plister = PropertyListEncoder()
             plister.outputFormat = .xml // just so we can read it
-            //print("attempting successfully to write an array of Items by encoding to plist first")
+            // writing an array of Items by encoding to plist first
             try plister.encode(itemArr).write(to: arrFile, options: .atomic)
-            //print("we didn't throw writing array of Items")
             let items = try String.init(contentsOf: arrFile)
-            //print(items) // show it as XML
         } catch {
             print(error)
         }
